2f29d0ea68e6c20f65a884a33f1b621e7520b5ad
nico.koltermann <nico.koltermann@tu-dortmund.de>
2022-10-16 01:06:43 +0100

    Remove old data

--- js/navigation.js
+++
@@ -1,8 +0,0 @@
-function initButtons() {
-    d3.select('#prev').on('click', () => {
-        location.href = "./";
-    });
-    d3.select('#next').on('click', () => {
-        location.href = "./plot-2";
-    });
-}
