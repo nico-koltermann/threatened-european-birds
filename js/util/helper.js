11162470530814c6def39f3627078985e8b46d1e
nico-koltermann <nico.koltermann@tu-dortmund.de>
2022-10-25 04:01:51 +0100

    Add helper

--- js/util/helper.js
+++ js/util/helper.js
@@ -7,3 +7,11 @@ function getColor(item) {
     return col;
   }
 }
+
+function comparePop(a, b) {
+  if (a.population_maximum_size >= b.population_maximum_size) {
+    return -1;
+  }
+  // a must be equal to b
+  return 1;
+}
