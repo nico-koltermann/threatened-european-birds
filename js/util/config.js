76b0bb7457498388811462762ff8069f5697b178
nico-koltermann <nico.koltermann@tu-dortmund.de>
2022-10-25 04:02:46 +0100

    Extend color for false data

--- js/util/config.js
+++ js/util/config.js
@@ -1,12 +1,15 @@
 
   let redListCatColorScale = {
     "CR": "#D90000",
-    "EN ": "#FF8400",
+    "CR ": "#D90000",
     "EN": "#FF8400",
-    "VU ": "#FFD740",
+    "EN ": "#FF8400",
     "VU": "#FFD740",
-    "NT ": "#7AD600",
+    "VU ": "#FFD740",
     "NT": "#7AD600",
+    "NT ": "#7AD600",
     "LC": "#006400",
-    "Unknown": "#808080"
+    "LC ": "#006400",
+    "Unknown": "#808080",
+    "Unknown ": "#808080"
   }
